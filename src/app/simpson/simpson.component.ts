cd40aa86c13a73d3c2c48072d4a74135cc3e76aa
Omarotierra <142826857+Omarotierra@users.noreply.github.com>
2023-11-02 19:17:31 -0600

    t finished (#23)

--- src/app/simpson/simpson.component.ts
+++ src/app/simpson/simpson.component.ts
@@ -11,14 +11,4 @@ export class SimpsonComponent {
   simpson(x0: number, x1: number, num_segmento: number, error: number, f: (x: number) => number): number {
     return this.calculator.simpson(x0, x1, num_segmento, error, f);
   }
-
-  t(x: number, dof: number) : number{
-    return this.calculator.t(x, dof);
-  }
-
-  gamma(x: number): number {
-    return this.calculator.gamma(x);
-  }
-
-
 }
